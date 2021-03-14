9b2ec17a54df810aa9f80c3062e061ad59523999
Siranov Iskander <64273077+M9sneek@users.noreply.github.com>
2021-03-14 14:27:30 +0600

    Add files via upload

---
+++ MouseLook.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float MouseSensitivity = 50.0f;
+    public Transform player;
+    float xRotation = 0f;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public CharacterController controller;
+    public float gravity = -10.0f;
+    public Transform groundCheck;
+    public float groundDistance = 0.5f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    void Update()
+    {   
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -2.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 velocity;
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+        
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltatime);
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 10.0f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 5.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            speed = 2.0f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            speed = 5.0f;
+        }
+    }
+}
